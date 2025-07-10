61c444a9014e0bbfd89b94acb40f6a2a64728183
lawalabdulrazaq <temitopelawal925@gmail.com>
2025-07-10 13:38:44 -0700

    anchor amm

---
+++ anchor_amm/programs/anchor_amm/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("ARpiPhZtekKF37DFyAQgZNDd3tSD98h1YFNAJ7SqKCV");
+
+#[program]
+pub mod anchor_amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
+}
